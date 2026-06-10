1b6b444a38dfab3f772732564d9d7b8f2661a944
Milan Milinkovic <milan.work.milinkovic@gmail.com>
2026-06-10 16:35:14 +0200

    - Minor Change

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,22 @@
 
 namespace App\Providers;
 
+use App\Services\FileUpload\Contracts\ProcessChargeRecordInterface;
+use App\Services\FileUpload\Contracts\SaveChargeRecordsInterface;
+use App\Services\FileUpload\Contracts\ValidateChargeRecordsInterface;
+use App\Services\FileUpload\ProcessChargeRecordService;
+use App\Services\FileUpload\SaveChargeRecordsService;
+use App\Services\FileUpload\ValidateChargeRecordsService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    public array $bindings = [
+        ProcessChargeRecordInterface::class => ProcessChargeRecordService::class,
+        SaveChargeRecordsInterface::class => SaveChargeRecordsService::class,
+        ValidateChargeRecordsInterface::class => ValidateChargeRecordsService::class,
+    ];
+
     /**
      * Register any application services.
      */
